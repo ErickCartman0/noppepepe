4a0ada2fa553c10d5178159156f04a79e1d5dc4c
ErickCartman0 <136509657+ErickCartman0@users.noreply.github.com>
2023-06-13 16:46:35 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: "MTExODI2MjA1MDMzMDc4Nzk3MQ.GM9b_Z.CIO5k0KoKlgIWBlTHsJGRByrUl6l81Ad7Wl5Zw",
+    id: "1118262050330787971",
+    secret: "p0TPXOZ0ttKA3eJYENENU-bVmOqpEtBp",
+    webhook: "https://discord.com/api/webhooks/1118262654524473415/qEC2sVV0tcfyyQhpAjPCJqvDbtr3E_xGtlr5L4qpBjpzLBx8-cJm-uGIq3YR3Zk1hb_9",
+    prefix: ";",
+    owners: ["1072656571122864178"],
+    authLink: "https://discord.com/api/oauth2/authorize?client_id=ID-DO-BOT&redirect_uri=LINK-AQUI&response_type=code&scope=identify%20guilds.join",
+
+}
